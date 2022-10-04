6a5935470814c75e49d7e97f8ea96aa0c446fc3c
benquang <114910044+benquang@users.noreply.github.com>
2022-10-04 17:49:35 +0700

    Create footer.php

---
+++ templates/includes/footer.php
@@ -0,0 +1,37 @@
+  <!--================ start footer Area  =================-->
+  <footer class="footer-area section_gap">
+    <div class="container">
+      <div class="footer-bottom row align-items-center">
+        <p class="footer-text m-0 col-lg-8 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+        <div class="col-lg-4 col-md-12 footer-social">
+          <a href="#"><i class="fa fa-facebook"></i></a>
+          <a href="#"><i class="fa fa-twitter"></i></a>
+          <a href="#"><i class="fa fa-dribbble"></i></a>
+          <a href="#"><i class="fa fa-behance"></i></a>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--================ End footer Area  =================-->
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="static/js/jquery-3.2.1.min.js"></script>
+  <script src="static/js/popper.js"></script>
+  <script src="static/js/bootstrap.min.js"></script>
+  <script src="static/js/stellar.js"></script>
+  <script src="static/vendors/lightbox/simpleLightbox.min.js"></script>
+  <script src="static/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+  <script src="static/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+  <script src="static/vendors/isotope/isotope-min.js"></script>
+  <script src="static/vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="static/js/jquery.ajaxchimp.min.js"></script>
+  <script src="static/vendors/counter-up/jquery.waypoints.min.js"></script>
+  <script src="static/vendors/counter-up/jquery.counterup.js"></script>
+  <script src="static/js/mail-script.js"></script>
+  <script src="static/js/theme.js"></script>
+</body>
+
+</html>
