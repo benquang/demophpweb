fe365f3e8cb76721afd61e49646772a1c972b93a
benquang <114910044+benquang@users.noreply.github.com>
2022-10-04 17:49:44 +0700

    Add files via upload

---
+++ templates/includes/header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="icon" href="img/favicon.png" type="image/png" />
+  <title>Mister's Imagine Toystore</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="static/css/bootstrap.css" />
+  <link rel="stylesheet" href="static/vendors/linericon/style.css" />
+  <link rel="stylesheet" href="static/css/font-awesome.min.css" />
+  <link rel="stylesheet" href="static/css/themify-icons.css" />
+  <link rel="stylesheet" href="static/css/flaticon.css" />
+  <link rel="stylesheet" href="static/vendors/owl-carousel/owl.carousel.min.css" />
+  <link rel="stylesheet" href="static/vendors/lightbox/simpleLightbox.css" />
+  <link rel="stylesheet" href="static/vendors/nice-select/css/nice-select.css" />
+  <link rel="stylesheet" href="static/vendors/animate-css/animate.css" />
+  <link rel="stylesheet" href="static/vendors/jquery-ui/jquery-ui.css" />
+  <!-- main css -->
+  <link rel="stylesheet" href="static/css/style.css" />
+  <link rel="stylesheet" href="static/css/responsive.css" />
+</head>
+
+<body>
+<header class="header_area">
+    <div class="top_menu">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7">
+            <div class="float-left">
+              <p>Phone: +01 1963 1967</p>
+              <p>email: misterimaginetoystore@gmail.com</p>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <div class="float-right">
+              <ul class="right_side">
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main_menu">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light w-100">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <a class="navbar-brand logo_h" href="/">
+            <img src="static/img/logochinhthuc1.png" alt="" />
+          </a>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse offset w-100" id="navbarSupportedContent">
+            <div class="row w-100 mr-0">
+              <div class="col-lg-7 pr-0">
+                <ul class="nav navbar-nav center_nav pull-right">
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Store</a>
+                  </li>
+
+                  <li class="nav-item submenu dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                      aria-expanded="false">Shop</a>
+                    <ul class="dropdown-menu">
+                      <li class="nav-item">
+                        <a class="nav-link" href="/">Shopping Cart</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/">Product Checkout</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">Blog</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/">profile</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-5 pr-0">
+                <ul class="nav navbar-nav navbar-right right_nav pull-right">
+                    <!-- -->
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
